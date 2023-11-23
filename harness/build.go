9b6987de5dc5a9be0b1ab59a623a7fd2b3f86931
mfk <mfk@hengwei.com.cn>
2023-11-23 15:43:46 +0800

    fix build.go

--- harness/build.go
+++ harness/build.go
@@ -102,7 +102,7 @@ func Build(buildFlags ...string) (app *App, compileError *revel.Error) {
 
 	pkg, err := build.Default.Import(revel.ImportPath, "", build.FindOnly)
 	if err != nil {
-		revel.RevelLog.Fatal("Failure importing", "path", revel.ImportPath)
+		revel.RevelLog.Fatal("Failure importing", "path", revel.ImportPath, "err", err)
 	}
 
 	// Binary path is a combination of $GOBIN/revel.d directory, app's import path and its name.
@@ -148,7 +148,7 @@ func Build(buildFlags ...string) (app *App, compileError *revel.Error) {
 			copy(buildCmd.Env, os.Environ())
 			buildCmd.Env = append(buildCmd.Env, "GOOS="+goos)
 		}
-		revel.RevelLog.Debug("Exec:", "args", buildCmd.Args)
+		revel.RevelLog.Info("Exec:", "args", buildCmd.Args)
 		output, err := buildCmd.CombinedOutput()
 
 		// If the build succeeded, we're done.
@@ -187,7 +187,7 @@ func Build(buildFlags ...string) (app *App, compileError *revel.Error) {
 			} else {
 				getCmd = exec.Command(goPath, "get", pkgName)
 			}
-			revel.RevelLog.Debug("Exec:", "args", getCmd.Args)
+			revel.RevelLog.Info("Exec:", "args", getCmd.Args)
 			getOutput, err := getCmd.CombinedOutput()
 			if err != nil {
 				revel.RevelLog.Error(string(getOutput))
